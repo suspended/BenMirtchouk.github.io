efed6758f1e3aa62882477db87c2e8d0d03b3258
Ben Mirtchouk <benya.usa@gmail.com>
2016-02-25 18:09:30 -0500

    fb10

--- facebook.js
+++ facebook.js
@@ -1,14 +1,13 @@
-//heyoo
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  console.log("ayy lmao")
   js.src = "//connect.facebook.net/en_US/sdk.js";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 window.fbAsyncInit = function() {
+  console.log("ayy lmao");
   FB.init({
     appId      : '158436701207354',
     cookie     : true,  // enable cookies to allow the server to access 
