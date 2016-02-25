c941e083160597f18d25c5a2aff987ed654da74f
Ben Mirtchouk <benya.usa@gmail.com>
2016-02-25 17:35:23 -0500

    fb7

--- facebook.js
+++ facebook.js
@@ -1,3 +1,11 @@
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 window.fbAsyncInit = function() {
   FB.init({
     appId      : '158436701207354',
@@ -9,14 +17,6 @@ window.fbAsyncInit = function() {
 }
 
 
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
 FB.api(
     "/{1099989246698504}",
