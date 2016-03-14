24bf0761339798f31981f48246b8ecfea0b59d85
Ben Mirtchouk <benya.usa@gmail.com>
2016-03-14 12:30:46 -0400

    jump

---
+++ jump/asd.php
@@ -0,0 +1,6 @@
+<?php for ($i=0;$i<20;$i++){?>
+
+<iframe width="420" height="315" src="https://www.youtube.com/embed/J0C-2gBmTMs?version=3&autoplay=0&loop=1&playlist=J0C-2gBmTMs&t=<?php echo $i; ?>" frameborder="0">
+</iframe>
+
+<?php } ?>
