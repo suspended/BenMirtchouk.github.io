86cbb40d18ee428ab9405be4e5c918cf8ae5a440
Ben Mirtchouk <benya.usa@gmail.com>
2016-05-08 17:34:14 -0400

    wink

--- home/hover.js
+++ home/hover.js
@@ -1,4 +1,3 @@
-
 function onhover(){
 	$("#BenMirt").hover(
 		function() {
@@ -6,11 +5,37 @@ function onhover(){
 			$(".name").css("display","block");
 			$( ".name" ).removeClass("zoomOut");
 			$( ".name" ).addClass("zoomIn");
-			startBlink();
+			if (!blinking){
+				blinking=true;
+				startBlink();
+			}
 		}, function() {
 			$( this ).removeClass("pulse animated");
 			$( ".name" ).removeClass("zoomIn");
 			$( ".name" ).addClass("zoomOut");
+			blinking=false;
 		}
 	);
+}
+counter=0;
+blinking=false;
+colors=["red","orange","yellow","green","blue","indigo", "purple"];
+function startBlink(){
+	if (blinking){
+		var num;
+		for (num=1;num<8;num++){
+	    	change(num);
+		}
+
+		counter++;
+		setTimeout( startBlink, 80 );
+	}
+}
+
+function change(i){
+	if (counter==7) counter=0;
+	$( "#anytime"+i ).css("color",colors[(i+counter)%colors.length]);
+
+
+
 }
