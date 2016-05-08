0cd576a32c8bc17334f167b42879e7fde3c2debd
Ben Mirtchouk <benya.usa@gmail.com>
2016-05-08 15:53:35 -0400

    zoom

--- home/hover.js
+++ home/hover.js
@@ -1,9 +1,12 @@
+
 function onhover(){
 	$("#BenMirt").hover(
 		function() {
 			$( this ).addClass("pulse animated");
+			$(".name").css("display","block");
 		}, function() {
 			$( this ).removeClass("pulse animated");
+			$(".name").css("display","none");
 		}
 	);
 }
