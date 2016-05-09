a5929e7374c8b179a0c1140acb2331ec4e7bdc29
Ben Mirtchouk <benya.usa@gmail.com>
2016-05-09 11:09:02 -0400

    igs

---
+++ igs/js/index.js
@@ -0,0 +1,16 @@
+// Script to control the counter:
+// https://github.com/cnanney/css-flip-counter/blob/master/js/flipcounter.js
+
+var date=new Date();
+
+var igs=new Date(date.getFullYear(),date.getMonth(),date.getDate(),6,56,0,0);
+
+if (date.getTime()>igs.getTime()){
+	igs=new Date(date.getFullYear(),date.getMonth(),date.getDate()+1,6,56,0,0);
+}
+
+var defaults = {
+  value: parseInt((igs.getTime()-date.getTime())/1000,10), inc: -1, pace: 1000, auto: true
+};
+
+var c1 = new flipCounter('c1', defaults);
