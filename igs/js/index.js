1a11275f0f1dc6db02748442ea791e1045527075
Ben Mirtchouk <benya.usa@gmail.com>
2016-05-09 17:48:06 -0400

    igs-sat/sun

--- igs/js/index.js
+++ igs/js/index.js
@@ -9,6 +9,10 @@ if (date.getTime()>igs.getTime()){
 	igs=new Date(date.getFullYear(),date.getMonth(),date.getDate()+1,7,56,0,0);
 }
 
+if (igs.getDay()==0) igs=new Date(igs.getFullYear(),igs.getMonth(),igs.getDate()+1,7,56,0,0);
+if (igs.getDay()==6) igs=new Date(igs.getFullYear(),igs.getMonth(),igs.getDate()+2,7,56,0,0);
+
+
 var defaults = {
   value: parseInt((igs.getTime()-date.getTime())/1000,10), inc: -1, pace: 1000, auto: true
 };
