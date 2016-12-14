0fd08fe6db348ced5f25fa4ca59faefc0ff6ae6e
BenMirtchouk <benya.usa@gmail.com>
2016-12-13 19:01:35 -0500

    select person

--- Facebook/main.js
+++ Facebook/main.js
@@ -8,8 +8,42 @@ function e_log(a){
 dates=[];
 messages=[];
 people=[];
+chats=[[],[]];
+function pick_person(){
+	parse(
+		function(){
+			var divs=doc.firstChild.children[1].children[1].children;
+			for (var i=1;i<divs.length;i++){
+				for (var j=0;j<divs[i].children.length;j++){
+					convo=divs[i].children[j];
+					var convoText=convo.innerHTML;
+					people=convoText.substr(0,convoText.indexOf('<'));
+					if (chats[0].indexOf(people)!=-1){
+						chats[1][chats[0].indexOf(people)].push([i,j])
+					}else{
+						chats[0].push(people);
+						chats[1].push([[i,j]]);	
+					}
+				}
+			}
 
-function start(){
+			for (var i=0;i<chats[0].length;i++){
+				// var arr="";
+				// for (var j=0;j<chats[1][i].length;j++){
+				// 	arr+="["+chats[1][i][j][0]+","+chats[1][i][j][1]+"]";
+				// }
+
+				document.getElementById("select").innerHTML+="<option value='"+i+"'>"+chats[0][i]+"</option><br>";
+			}
+
+
+			document.getElementById("tohide").style["display"]="block";
+		}
+	);
+	
+}
+
+function parse(_callback){
 	log("starting...");
 	messages=[[],[]];
 	var temp=[];
@@ -22,21 +56,38 @@ function start(){
 		var parser = new DOMParser();
 		doc = parser.parseFromString(string, "text/html");
 		log("parsed file");
+		_callback(); 
+	});
+	reader.readAsText(document.querySelector('input').files[0]);
+}
 
-		mpeople=[];
-		mpeople2=[];
+function start(){
+	messages[0]=[];
+	messages[1]=[];
+	mpeople=[];
+	mpeople2=[];
 
-		convo=doc.firstChild.children[1].children[1].children[4].children[23];
+	cval=$("#select").val();
+	current=chats[1][cval];
+
+	var tmp_people=chats[0][cval];
+	tmp_people=tmp_people.split(',');
+	tmp_people[1]=tmp_people[1].substr(1);
+
+	mpeople[tmp_people[0]]=0;
+	mpeople[tmp_people[1]]=0;
+	mpeople2[tmp_people[0]]=0;
+	mpeople2[tmp_people[1]]=0;
+
+	for (var fragment=0;fragment<current.length;fragment++){
+		cfrag=current[fragment];
+		convo=doc.firstChild.children[1].children[1].children[cfrag[0]].children[cfrag[1]];
 		var convoText=convo.innerHTML;
 		people=convoText.substr(0,convoText.indexOf('<'));
 		people=people.split(',');
 		people[1]=people[1].substr(1);
-		log("found people");
+		log("found people "+people[0]+" and "+people[1]);
 
-		mpeople[people[0]]=0;
-		mpeople[people[1]]=0;
-		mpeople2[people[0]]=0;
-		mpeople2[people[1]]=0;
 
 		time=convo.children[convo.children.length-(convo.children.length%2==0 ? 2:1)].firstChild.children[1].innerHTML;
 		time=time.substr(time.indexOf(',')+2,time.indexOf(' at ')-time.indexOf(',')-2);
@@ -63,34 +114,23 @@ function start(){
 				toPush2={x: new Date(time), y: mpeople2[people[0]] + mpeople2[people[1]]};
 			}
 		}
-		log("gathered messages");
-		
-		drawGraph();
-	});
-	reader.readAsText(document.querySelector('input').files[0]);
-
-
-	// dates=[];
-	// var a=$("#start").val();
-	// var b=$("#end").val();
-	// if (a==""){
-	// 	e_log("no start date");
-	// 	return;
-	// }
-	// if (b=="") b=new Date();
-	// else try{
-	// 	b=new Date(b);
-	// }catch(err){e_log("b is not a date"); return;}
-	// try{
-	// 	a=new Date(a);
-	// }catch(err){e_log("a is not a date"); return;}
-
+	}
+	log("gathered messages");
 	
+	drawGraph();
+}
 
+function reset(){
+	$('#messages').remove();
+	$('#message_container').append('<canvas id="messages" class="messages"></canvas>');
 }
 
 function drawGraph(){
+	log("reseting...");
+	reset();
 	log("plotting graph...");
+
+	document.getElementById("messages").style["display"]="block";
 	var ctx = $(".messages");
 
 	var graphsData={
@@ -105,14 +145,15 @@ function drawGraph(){
 					type: 'line',
 					data: messages[1],
 					backgroundColor: 'rgba(1,212,255,.1)',
-		            borderColor: 'rgba(1,212,255,.5)'
+		            borderColor: 'rgba(1,212,255,.5)',
+		            lineTension: 0
 				},
 				{
 					label: 'per day',	    
 					type: 'line',
 					data: messages[0],
-					backgroundColor: 'rgba(1,212,255,.1)',
-		            borderColor: 'rgba(1,212,255,.5)',
+					backgroundColor: 'rgba(226,78,78,.1)',
+		            borderColor: 'rgba(226,78,78,.5)',
 		            lineTension: 0
 				}
 				
@@ -146,5 +187,5 @@ function drawGraph(){
 	    }
 	};
 	console.log(graphsData);
-	var myChart = new Chart(ctx, graphsData);
+	myChart = new Chart(ctx, graphsData);
 }
